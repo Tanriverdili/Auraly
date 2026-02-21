43ed19f36600e4451a6abebf52051e69e26db8be
Tanriverdili <gulcantanriverdiyeva@gmail.com>
2026-02-21 19:16:09 +0400

    Refactor UserRepository.java to remove blank lines
    Removed unnecessary blank lines for cleaner code.

--- src/main/java/com/example/auraly/repository/UserRepository.java
+++ src/main/java/com/example/auraly/repository/UserRepository.java
@@ -1,12 +1,7 @@
 package com.example.auraly.repository;
-
-
-
-
 import com.example.auraly.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 
